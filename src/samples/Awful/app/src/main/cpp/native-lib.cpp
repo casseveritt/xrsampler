9f9f7522434dfc68d9b56083124ef668170c1b19
Lucas Everitt <lucas.everitt@gmail.com>
2023-10-19 20:48:53 -0500

    Testing OpenXR function calls

--- src/samples/Awful/app/src/main/cpp/native-lib.cpp
+++ src/samples/Awful/app/src/main/cpp/native-lib.cpp
@@ -8,5 +8,7 @@ Java_us_xyzw_awful_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    PFN_xrInitializeLoaderKHR xrInitializeLoaderKHR = nullptr;
+    xrGetInstanceProcAddr(XR_NULL_HANDLE, "xrInitializeLoaderKHR", reinterpret_cast<PFN_xrVoidFunction*>(&xrInitializeLoaderKHR));
     return env->NewStringUTF(hello.c_str());
 }
